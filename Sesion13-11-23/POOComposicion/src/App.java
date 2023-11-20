191683d6c606dade7845c38600b01943f7d77add
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-19 19:01:10 -0500

    App.java

--- Sesion13-11-23/POOComposicion/src/App.java
+++ Sesion13-11-23/POOComposicion/src/App.java
@@ -1,5 +1,22 @@
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        TarjetaMadre miTarjetaMadre =new TarjetaMadre("Gigabyte 750", "Gigabyte", 4, 3, "Pheonix");
+
+        Resolucion resolucionMonitor = new Resolucion(1440, 900);
+
+        Monitor miMonitor = new Monitor("ThinkVision", "Lenovo", 15.3, resolucionMonitor);
+
+        Dimensiones dimensionesCASE = new Dimensiones(173, 425, 275);
+
+        Case miCase = new Case("AL-K34", "Aluna", "Platinium", dimensionesCASE);
+        
+        Computadora miPC = new Computadora(miCase, miMonitor, miTarjetaMadre);
+
+        miPC.mostrarElCase().presionarBotonEncendido();
+        miPC.mostrarMonitor().dibujarPixel(5, 100, "Naranja");
+        miPC.mostrarTarjetaMadre().cargarPrograma("Visual Studio Code");
     }
 }
